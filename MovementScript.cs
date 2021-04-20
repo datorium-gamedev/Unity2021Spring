ab62a0febbc13e5c14eeb0f1732c517714f7ac98
Keron320 <32654913+Keron320@users.noreply.github.com>
2021-04-20 20:07:33 +0300

    20042021

---
+++ MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public int playerSpeed = 1;
+    private float turnSpeed = 1.53453453453450f;
+    protected bool canMove = false;
+
+    public Rigidbody myRigid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") * playerSpeed;
+        float verticalInput = Input.GetAxis("Vertical") * playerSpeed;
+
+        Vector3 forwardMovement = gameObject.transform.forward * verticalInput * Time.deltaTime;
+        Vector3 strifeMovement = gameObject.transform.right * horizontalInput * Time.deltaTime;
+
+        gameObject.transform.Translate(forwardMovement);
+        gameObject.transform.Translate(strifeMovement);
+        
+
+    }
+}

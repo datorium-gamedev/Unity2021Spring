647bde0244178df28061d6fa52e07eeb346b94d6
Keron320 <32654913+Keron320@users.noreply.github.com>
2021-04-27 19:47:54 +0300

    2704

---
+++ MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public int mouseSensitivity = 300;
+    private float xClamp;
+    public Camera myCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        gameObject.GetComponent<Transform>().Rotate(Vector3.up * MouseX);
+        xClamp = Mathf.Clamp(xClamp - MouseY, -90, 90);
+        myCamera.transform.eulerAngles = new Vector3(xClamp, myCamera.transform.eulerAngles.y, myCamera.transform.eulerAngles.z);
+
+    }
+}
--- MovementScript.cs
+++ MovementScript.cs
@@ -8,10 +8,13 @@ public class MovementScript : MonoBehaviour
     private float turnSpeed = 1.53453453453450f;
     protected bool canMove = false;
 
-    public Rigidbody myRigid;
+    public bool onGround = true;
+    public float YForce = 20f;
 
-    // Update is called once per frame
-    void Update()
+    public Rigidbody playerRigid;
+
+// Update is called once per frame
+void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal") * playerSpeed;
         float verticalInput = Input.GetAxis("Vertical") * playerSpeed;
@@ -19,9 +22,18 @@ public class MovementScript : MonoBehaviour
         Vector3 forwardMovement = gameObject.transform.forward * verticalInput * Time.deltaTime;
         Vector3 strifeMovement = gameObject.transform.right * horizontalInput * Time.deltaTime;
 
-        gameObject.transform.Translate(forwardMovement);
-        gameObject.transform.Translate(strifeMovement);
-        
+
+        gameObject.transform.Translate(Vector3.right * verticalInput * playerSpeed * Time.deltaTime);
+        gameObject.transform.Translate(Vector3.back * horizontalInput * playerSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && onGround == true)
+        {
+            playerRigid.AddForce(new Vector3(0, YForce, 0), ForceMode.Impulse);
+            //onGround = false;
+        }
+
+
 
     }
 }
